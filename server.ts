bc69418456cea553a232021e2b8516686229516f
zainahmed768 <zainahmed231@gmail.com>
2024-05-31 18:49:12 +0500

    Front is done

--- server.ts
+++ server.ts
@@ -3,11 +3,11 @@ import { config } from "./src/config/config";
 import connectDB from "./src/config/db";
 
 const startServer = async () => {
-  await connectDB();
-  const port = config.port || 3000;
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+	await connectDB();
+	const port = config.port || 3000;
+	app.listen(port, () => {
+		console.log(`Server listening on port ${port}`);
+	});
 };
 
 startServer();
--- src/config/db.ts
+++ src/config/db.ts
@@ -2,19 +2,25 @@ import mongoose from "mongoose";
 import { config } from "./config";
 
 const connectDB = async () => {
-  try {
-    mongoose.connection.on("connected", () => {
-      console.log("MongoDB connected");
-    });
+	try {
+		mongoose.connection.on("connected", () => {
+			console.log("MongoDB connected");
+		});
 
-    mongoose.connection.on("error", (error) => {
-      console.log("MongoDB connection error: ", error);
-    });
-    await mongoose.connect(config.databaseUrl as string);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+		mongoose.connection.on("error", (error) => {
+			console.log("MongoDB connection error: ", error);
+		});
+		const options = {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+		};
+		await mongoose.connect(config.databaseUrl as string, options as any);
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 export default connectDB;
